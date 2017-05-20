cd0058bda94a15f70a60115377756c8ec6caa9e9
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-20 09:52:45 +0200

    add time stample as well

--- src/test/java/com/github/marschall/javaone2017/JdbcConversion.java
+++ src/test/java/com/github/marschall/javaone2017/JdbcConversion.java
@@ -3,10 +3,12 @@ package com.github.marschall.javaone2017;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.zone.ZoneOffsetTransition;
 import java.time.zone.ZoneRules;
@@ -16,7 +18,7 @@ import org.junit.Test;
 public class JdbcConversion {
 
   @Test
-  public void lossyConversion() {
+  public void timestampConversion() {
     ZoneId systemTimezone = ZoneId.systemDefault();
     Instant now = Instant.now();
 
@@ -29,10 +31,20 @@ public class JdbcConversion {
     }
 
     Duration gap = Duration.between(transition.getDateTimeBefore(), transition.getDateTimeAfter());
-    LocalDateTime notExisting = transition.getDateTimeBefore().plus(gap.dividedBy(2L));
+    LocalDateTime betweenTransitions = transition.getDateTimeBefore().plus(gap.dividedBy(2L));
 
-    Timestamp timestamp = java.sql.Timestamp.valueOf(notExisting);
-    assertEquals(notExisting, timestamp.toLocalDateTime());
+    Timestamp timestamp = java.sql.Timestamp.valueOf(betweenTransitions);
+    assertEquals(betweenTransitions, timestamp.toLocalDateTime());
+  }
+
+
+
+  @Test
+  public void timeConversion() {
+    LocalTime expected = LocalTime.of(9, 50, 4, 45789000);
+
+    Time time = java.sql.Time.valueOf(expected);
+    assertEquals(expected, time.toLocalTime());
   }
 
 }
